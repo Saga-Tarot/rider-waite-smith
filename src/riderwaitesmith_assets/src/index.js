f637775607279089edbf5d6967d3cb3f1074ac2a
Jorgen <jorgen@buildnode.io>
2021-10-13 12:22:53 -0700

    junk

--- src/riderwaitesmith_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { riderwaitesmith } from "../../declarations/riderwaitesmith";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with riderwaitesmith actor, calling the greet method
-  const greeting = await riderwaitesmith.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
